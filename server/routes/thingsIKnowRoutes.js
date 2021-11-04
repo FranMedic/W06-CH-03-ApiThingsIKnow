15487b59403659b942dd070600aaccb5304ff6ea
Franny Medico <m.francisca.medico@gmail.com>
2021-11-05 00:13:37 +0100

    added debug with chalk in routes

--- server/routes/thingsIKnowRoutes.js
+++ server/routes/thingsIKnowRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const thing = await Thing.find();
-
+  debug(chalk.green("loaded items"));
   res.json(thing);
 });
 
@@ -16,6 +16,7 @@ router.get("/:id", async (req, res, next) => {
   try {
     const searchedThing = await Thing.findById(id);
     if (searchedThing) {
+      debug(chalk.green("loaded item"));
       res.json(searchedThing);
     } else {
       const error = new Error("Thing not found");
@@ -33,23 +34,7 @@ router.delete("/:id", async (req, res, next) => {
   try {
     const searchedThing = await Thing.findByIdAndDelete(id);
     if (searchedThing) {
-      res.json("deleted");
-    } else {
-      const error = new Error("Thing not found");
-      error.code = 404;
-      throw error;
-    }
-  } catch (error) {
-    error.code = 400;
-    next(error);
-  }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const searchedThing = await Thing.findByIdAndDelete(id);
-    if (searchedThing) {
+      debug(chalk.green("deleted item"));
       res.json("deleted");
     } else {
       const error = new Error("Thing not found");
@@ -66,6 +51,7 @@ router.post("/", async (req, res, next) => {
   try {
     const thing = req.body;
     const newThing = await Thing.create(thing);
+    debug(chalk.green("created item"));
     res.json(newThing);
   } catch (error) {
     error.code = 400;
@@ -78,6 +64,7 @@ router.put("/", async (req, res, next) => {
   try {
     const { _id } = req.body;
     await Thing.findByIdAndUpdate(_id, req.body);
+    debug(chalk.green("modified item"));
     res.json(req.body);
   } catch (error) {
     error.code = 400;
